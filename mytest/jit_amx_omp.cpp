6202cfd7cf26da11ab2eefec1f0d87d6e3f47ec0
Luwei Zhou <luwei.zhou@intel.com>
2024-10-29 19:48:20 -0700

    Enable n block  when K inner most loop jit kernel.

--- mytest/jit_amx_omp.cpp
+++ mytest/jit_amx_omp.cpp
@@ -21,12 +21,13 @@
 #endif
 
 
-class Linear32x32_AMX : public jit_generator {
+class Linear32x32_AMX_K : public jit_generator {
 public:
-    int m_K;
+    int m_K, m_N;
     TileConfig m_tile_cfg;
-    Linear32x32_AMX(int K) : m_K(K) {
-        create_kernel("Linear32x32_AMX");
+    Linear32x32_AMX_K(int K, int N = 32) : m_K(K), m_N(N) {
+        create_kernel("Linear32x32_AMX_K");
+        assert(m_K % 32 == 0 && m_N % 32 == 0);
         m_tile_cfg.reset(1, 0,
                          {
                              {16, 64}, // C:0
@@ -43,14 +44,20 @@ public:
     const TileConfig& tile_config() { return m_tile_cfg; }
 
     // to save push/pop: do not use `abi_save_gpr_regs`
-    Xbyak::Reg64 reg_A_addr = abi_param1;
+    Xbyak::Reg64 reg_A_base = abi_param1;
     Xbyak::Reg64 reg_A_stride = abi_param2;
-    Xbyak::Reg64 reg_B_addr = abi_param3;
-    Xbyak::Reg64 reg_C_addr = abi_param4;
+    Xbyak::Reg64 reg_B_base = abi_param3;
+    Xbyak::Reg64 reg_C_base = abi_param4;
     Xbyak::Reg64 reg_C_stride = abi_param5;
     Xbyak::Reg64 reg_B_stride = r10;
     Xbyak::Reg64 reg_A1_addr = r11;
     Xbyak::Reg64 reg_ktiles = r9;
+    Xbyak::Reg64 reg_ntiles = r12;
+
+    Xbyak::Reg64 reg_A_addr = r13;
+    Xbyak::Reg64 reg_B_addr = r14;
+    Xbyak::Reg64 reg_C_addr = r15;
+
 
     Xbyak::Tmm tmmC00 = tmm0;
     Xbyak::Tmm tmmC10 = tmm1;
@@ -63,74 +70,92 @@ public:
 
 
     void generate() {
+        preamble();
+
         Xbyak::Label loop_k_tile;
-        assert(m_K % 32 == 0);
+        Xbyak::Label loop_n_tile;
+
+
+
+        mov(reg_B_stride, 64);
+        mov(reg_ntiles, m_N / 32);
+
+        const size_t step_a = 64;
+        const size_t step_b = 2048;
+        const size_t step_c = 64;
+
+        // const size_t offset_b = 2048*128/32;
+        const size_t offset_c = 32*4;
+
+        align(64, false);
+        bool do_sw_prefetch = std::getenv("SWPF") != nullptr;
+        mov(reg_B_addr, reg_B_base);
+        L(loop_n_tile);
         tilezero(tmmC00);
         tilezero(tmmC01);
         tilezero(tmmC10);
         tilezero(tmmC11);
-
-        mov(reg_B_stride, 64);
         mov(reg_ktiles, m_K / 32);
-        const size_t step_a = 64;
-        const size_t step_b = 2048;
-        const size_t step_c = 64;
-        align(64, false);
-        bool do_sw_prefetch = std::getenv("SWPF") != nullptr;
-
-        L(loop_k_tile);
-        if (do_sw_prefetch) {
-            for (auto cl = 0; cl < 16; cl++) {
-                auto cl_offset = 64 * cl;
-                prefetcht0(ptr[reg_A_addr + step_a + cl_offset]);
+        mov(reg_A_addr, reg_A_base);
+        mov(reg_C_addr, reg_C_base);
+        {
+            L(loop_k_tile);
+            if (do_sw_prefetch) {
+                for (auto cl = 0; cl < 16; cl++) {
+                    auto cl_offset = 64 * cl;
+                    prefetcht0(ptr[reg_A_addr + step_a + cl_offset]);
+                }
             }
-        }
-        tileloadd(tmmA0, ptr[reg_A_addr + reg_A_stride]);
-        tileloadd(tmmB0, ptr[reg_B_addr + reg_B_stride]);
-        tdpbf16ps(tmmC00, tmmA0, tmmB0);
+            tileloadd(tmmA0, ptr[reg_A_addr + reg_A_stride]);
+            tileloadd(tmmB0, ptr[reg_B_addr + reg_B_stride]);
+            tdpbf16ps(tmmC00, tmmA0, tmmB0);
 
-        if (do_sw_prefetch) {
-            for (auto cl = 0; cl < 16; cl++) {
-                auto cl_offset = 64 * cl;
-                prefetcht0(ptr[reg_B_addr + step_b + cl_offset]);
+            if (do_sw_prefetch) {
+                for (auto cl = 0; cl < 16; cl++) {
+                    auto cl_offset = 64 * cl;
+                    prefetcht0(ptr[reg_B_addr + step_b + cl_offset]);
+                }
             }
-        }
 
-        lea(reg_A1_addr, ptr[reg_A_addr + reg_A_stride * 8]);
-        lea(reg_A1_addr, ptr[reg_A1_addr + reg_A_stride * 8]);
-        if (do_sw_prefetch) {
-            for (auto cl = 0; cl < 16; cl++) {
-                auto cl_offset = 64 * cl;
-                prefetcht0(ptr[reg_A1_addr + step_a + cl_offset]);
+            lea(reg_A1_addr, ptr[reg_A_addr + reg_A_stride * 8]);
+            lea(reg_A1_addr, ptr[reg_A1_addr + reg_A_stride * 8]);
+            if (do_sw_prefetch) {
+                for (auto cl = 0; cl < 16; cl++) {
+                    auto cl_offset = 64 * cl;
+                    prefetcht0(ptr[reg_A1_addr + step_a + cl_offset]);
+                }
             }
-        }
-        tileloadd(tmmA1, ptr[reg_A1_addr + reg_A_stride]);
-        tdpbf16ps(tmmC10, tmmA1, tmmB0);
+            tileloadd(tmmA1, ptr[reg_A1_addr + reg_A_stride]);
+            tdpbf16ps(tmmC10, tmmA1, tmmB0);
 
-        if (do_sw_prefetch) {
-            for (auto cl = 0; cl < 16; cl++) {
-                auto cl_offset = 64 * cl;
-                 prefetcht0(ptr[reg_B_addr + 1024 +  step_b + cl_offset]);
+            if (do_sw_prefetch) {
+                for (auto cl = 0; cl < 16; cl++) {
+                    auto cl_offset = 64 * cl;
+                    prefetcht0(ptr[reg_B_addr + 1024 +  step_b + cl_offset]);
+                }
             }
+            tileloadd(tmmB1, ptr[reg_B_addr + reg_B_stride + 1024]);
+            tdpbf16ps(tmmC01, tmmA0, tmmB1);
+            tdpbf16ps(tmmC11, tmmA1, tmmB1);
+            lea(reg_A_addr, ptr[reg_A_addr + step_a]);
+            lea(reg_B_addr, ptr[reg_B_addr + step_b]);
+
+            dec(reg_ktiles);
+            jnz(loop_k_tile, T_NEAR);
+            //store C01, C01
+            tilestored(ptr[reg_C_addr + reg_C_stride], tmmC00);
+            tilestored(ptr[reg_C_addr + reg_C_stride + step_c], tmmC01);
+            // lea(reg_C_addr, ptr[reg_C_addr + reg_C_stride * 16]);
+            lea(reg_C_addr, ptr[reg_C_addr + reg_C_stride * 8]);
+            lea(reg_C_addr, ptr[reg_C_addr + reg_C_stride * 8]);
+            //store C10, C11
+            tilestored(ptr[reg_C_addr + reg_C_stride], tmmC10);
+            tilestored(ptr[reg_C_addr + reg_C_stride + step_c], tmmC11);
         }
-        tileloadd(tmmB1, ptr[reg_B_addr + reg_B_stride + 1024]);
-        tdpbf16ps(tmmC01, tmmA0, tmmB1);
-        tdpbf16ps(tmmC11, tmmA1, tmmB1);
-        lea(reg_A_addr, ptr[reg_A_addr + step_a]);
-        lea(reg_B_addr, ptr[reg_B_addr + step_b]);
-
-        dec(reg_ktiles);
-        jnz(loop_k_tile, T_NEAR);
-    
-        tilestored(ptr[reg_C_addr + reg_C_stride], tmmC00);
-        tilestored(ptr[reg_C_addr + reg_C_stride + step_c], tmmC01);
-        // lea(reg_C_addr, ptr[reg_C_addr + reg_C_stride * 16]);
-        lea(reg_C_addr, ptr[reg_C_addr + reg_C_stride * 8]);
-        lea(reg_C_addr, ptr[reg_C_addr + reg_C_stride * 8]);
-
-        tilestored(ptr[reg_C_addr + reg_C_stride], tmmC10);
-        tilestored(ptr[reg_C_addr + reg_C_stride + step_c], tmmC11);
-        ret();
+        lea(reg_C_base, ptr[reg_C_base + offset_c]);
+        dec(reg_ntiles);
+        jnz(loop_n_tile, T_NEAR);
+        postamble();
     }
 };
 
@@ -160,6 +185,8 @@ timeit timer({
 
 template <typename LinearAMX>
 int amx_jit(const int M, const int N, const int K, int times = -1000) {
+    int n_block = 64;
+    assert(N%n_block == 0);
     tensor2D<ov::bfloat16> A(M, K,
                              true); // ensure stride of A matrix is multiple of
                                     // cache line, which is vital to performance.
@@ -168,7 +195,7 @@ int amx_jit(const int M, const int N, const int K, int times = -1000) {
     tensor2D<ov::bfloat16> BPacked(K * N, 1, true);
     tensor2D<float> C0(M, N, true); // reference result
     tensor2D<float> C1(M, N, true); // actual result
-    LinearAMX mm_jit(K);
+    LinearAMX mm_jit(K, n_block);
     // TileConfigScope tcfg(mm_jit.tile_config());
     #pragma omp parallel
     {
@@ -190,8 +217,8 @@ int amx_jit(const int M, const int N, const int K, int times = -1000) {
     #pragma omp parallel for collapse(2)       //split M/32*N/32 acrosss cores.
     //#pragma omp parallel for                 //split M/32 across cores.
     for (int j = 0; j < M/32; j++) {
-        for (int i = 0; i < N/32; i++) {
-            mm_jit(&A(j*32,0), A.stride, &BPacked[i*32*K], &C1(j*32, i*32), C1.stride);
+        for (int i = 0; i < N/n_block; i++) {
+            mm_jit(&A(j*32,0), A.stride, &BPacked[i*n_block*K], &C1(j*32, i*n_block), C1.stride);
         }
     }
 
@@ -235,7 +262,6 @@ int main(int argc, const char* argv[]) {
 
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
     std::cout << "===============================BF16========================\n";
-    amx_jit<Linear32x32_AMX>(1024, 1024, 128);
-    amx_jit<Linear32x32_AMX>(4096, 4096, 128);
+    amx_jit<Linear32x32_AMX_K>(32, 1024, 512);
 
 }
